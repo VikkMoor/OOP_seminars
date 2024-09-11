694fd8348dc68f1ea6ad845ce233012019da16ea
VikkMoor <moorvikk@gmail.com>
2024-09-11 22:51:33 +0500

    task1

--- seminars/sem1/task1/Product.java
+++ seminars/sem1/task1/Product.java
@@ -1,8 +1,8 @@
 package sem1.task1;
 
 public class Product {
-    String name;
-    Integer price;
+    private String name;
+    private Integer price;
 
     public Product(String name, Integer price) {
         this.name = name;
--- seminars/sem1/task1/VendingMachine.java
+++ seminars/sem1/task1/VendingMachine.java
@@ -7,21 +7,26 @@ public class VendingMachine {
     private List<Product> productList;
     private Integer money;
 
+    public VendingMachine(List<Product> productList, Integer money) {
+        this.productList = productList;
+        this.money = 0;
+    }
+
     public Product getProduct(String name) {
         for (Product product: productList) {
-            if (product.name.equals(name)) {
-                money += product.price;
+            if (product.getName().equals(name)) {
+                money += product.getPrice();
                 return  product;
             }
         }
         return null;
     }
 
-    List<Product> initProduct() {
+    private List<Product> initProduct() {
         List<Product> products = new ArrayList<>();
-        products.add(new Product());
-        products.add(new Product());
-        products.add(new Product());
+        products.add(new Product("Milk", 100));
+        products.add(new Product("Water", 50));
+        products.add(new Product("Kirieshki", 30));
         return products;
     }
 
