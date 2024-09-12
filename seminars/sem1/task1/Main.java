648abe2f7ea52254ebce40ff8569b5421f2e1201
VikkMoor <moorvikk@gmail.com>
2024-09-12 21:37:57 +0500

    hw1 main

--- seminars/sem1/task1/Main.java
+++ seminars/sem1/task1/Main.java
@@ -1,9 +1,17 @@
 package sem1.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Meow");
-    }
+        List<Product> drinks = new ArrayList<>();
+        drinks.add(new HotDrink("Tea", 20, 60));
+        drinks.add(new HotDrink("Coffee", 30, 70));
+        drinks.add(new HotDrink("Hot Chocolate", 40, 50));
+        HotDrinkVendingMachine test = new HotDrinkVendingMachine(drinks, 500);
+        System.out.println();
 
+    }
 
 }
