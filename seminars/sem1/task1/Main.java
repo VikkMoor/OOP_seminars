997d8ebc9f4bf37b15ba36df8fd7e829063319c6
VikkMoor <moorvikk@gmail.com>
2024-09-13 15:18:33 +0500

    hw1 finished

--- seminars/sem1/task1/Main.java
+++ seminars/sem1/task1/Main.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Product> drinks = new ArrayList<>();
-        drinks.add(new HotDrink("Tea", 20, 60));
-        drinks.add(new HotDrink("Coffee", 30, 70));
-        drinks.add(new HotDrink("Hot Chocolate", 40, 50));
+        drinks.add(new HotDrink("Tea", 20, 200, 75));
+        drinks.add(new HotDrink("Coffee", 30, 250, 70));
+        drinks.add(new HotDrink("Hot Chocolate", 40, 300, 65));
         HotDrinkVendingMachine test = new HotDrinkVendingMachine(drinks, 500);
         System.out.println(test);
-        System.out.println(test.getProduct("Tea", 20, 60));
+        System.out.println(test.getProduct("Tea", 200, 75));
     }
 
 }
