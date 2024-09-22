58ccf1396f1c2c03b3daf41dd0a303dfc844ec84
VikkMoor <moorvikk@gmail.com>
2024-09-22 08:22:16 +0500

    hw3 streamService edited

--- seminars/sem3/StreamService.java
+++ seminars/sem3/StreamService.java
@@ -5,29 +5,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class StreamService {
-    public class StreamService {
 
-        // Поле для хранения списка потоков
-        private List<Stream> streams;
+    private List<Stream> streams;
 
-        // Конструктор для инициализации списка потоков
-        public StreamService() {
-            this.streams = new ArrayList<>();
-        }
+    public StreamService() {
+        this.streams = new ArrayList<>();
+    }
 
-        // Метод для получения списка потоков
-        public List<Stream> getStreams() {
-            return streams;
-        }
+    public List<Stream> getStreams() {
+        return streams;
+    }
 
-        // Метод для добавления потока в список
-        public void addStream(Stream stream) {
-            this.streams.add(stream);
-        }
+    public void addStream(Stream stream) {
+        this.streams.add(stream);
+    }
 
-        // Метод для сортировки потоков
-        public void sortStreams() {
-            Collections.sort(streams, new StreamComparator());
-        }
+    // Sort stream method:
+    public void sortStreams() {
+        Collections.sort(streams, new StreamComparator());
     }
 }
