bdeea54324220e59f639d087081814a4201bd4ee
VikkMoor <moorvikk@gmail.com>
2024-09-22 18:36:51 +0500

    sem4 UserComparator

---
+++ seminars/sem4/Student.java
@@ -0,0 +1,18 @@
+package sem4;
+
+public class Student extends User implements Comparable<Student> {
+    private Long studentId;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.studentId.compareTo(o.studentId);
+    }
+}
